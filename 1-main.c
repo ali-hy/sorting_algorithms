ad232a02b7be6a720e223637b409cd2f068e5258
ali-hy <ali.haitham.ymy@gmail.com>
2024-02-17 17:08:33 +0200

    Revert "Revert "I hate the checker""
    This reverts commit 217b575290ca01ea3872b98feca475df951868fb.

--- 1-main.c
+++ 1-main.c
@@ -34,24 +34,62 @@ listint_t *create_listint(const int *array, size_t size)
 }
 
 /**
+ * test_insertion_sort_list - what do u want
+ */
+void test_insertion_sort_list(void)
+{
+	/* Array of test case sizes, represented as constants.*/
+	const int test_case_sizes[] = {0, 1, 1, 2, 3, 5, 5, 5, 5, 4, 6, 7};
+	int a1[] = {0},				/*Empty list*/
+		a2[] = {42},			/*Single-element list*/
+		a3[] = {42, 42},		/*Identical elements*/
+		a4[] = {42, 21, 42},	/*Duplicate elements*/
+		a5[] = {5, 4, 3, 2, 1}, /*Reversed order*/
+		a6[] = {1, 2, 3, 4, 5}, /*Already sorted*/
+		a7[] = {5, 1, 4, 2, 3}, /*Mixed order*/
+		/*Additional test cases:*/
+		a8[] = {-5, 2, 9, -1, 4},	   /*Negative numbers*/
+		a9[] = {10000, 1, 0, -10000},  /*Large and small numbers*/
+		a10[] = {1, 3, 2, 5, 4, 6},	   /*Even-sized list*/
+		a11[] = {1, 3, 2, 5, 4, 6, 7}, /*Odd-sized list*/
+			**test_cases = malloc(12 * sizeof(int *));
+	int num_test_cases = 11;
+	int i;
+	listint_t *list = NULL;
+
+	test_cases[1] = a1;
+	test_cases[2] = a2;
+	test_cases[3] = a3;
+	test_cases[4] = a4;
+	test_cases[5] = a5;
+	test_cases[6] = a6;
+	test_cases[7] = a7;
+	test_cases[8] = a8;
+	test_cases[9] = a9;
+	test_cases[10] = a10;
+	test_cases[11] = a11;
+
+	for (i = 1; i <= num_test_cases; i++)
+	{
+		printf("Testcase #%d - ", i);
+		list = create_listint(test_cases[i], test_case_sizes[i]);
+		print_list(list);
+		printf("\n");
+		insertion_sort_list(&list);
+		printf("\n");
+		print_list(list);
+		printf("\n");
+	}
+}
+
+/**
  * main - Entry point
  *
  * Return: Always 0
  */
 int main(void)
 {
-	listint_t *list;
-	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-	size_t n = sizeof(array) / sizeof(array[0]);
-
-	list = create_listint(array, n);
-	if (!list)
-		return (1);
-	print_list(list);
-	printf("\n");
-	insertion_sort_list(&list);
-	printf("\n");
-	print_list(list);
+	test_insertion_sort_list();
 
 	return (0);
 }
