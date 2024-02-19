5cd0e9843d2649642b21b86ff43b88abe7c1ce56
ali-hy <ali.haitham.ymy@gmail.com>
2024-02-19 22:55:48 +0200

    3-quick_sort.c

---
+++ 3-quick_sort.c
@@ -0,0 +1,66 @@
+#include "sort.h"
+#include <unistd.h>
+
+/**
+ * partition - resolves one partition in quick sort
+ * @arr: array to sort
+ * @low: ...
+ * @high: ...
+ * @size: size of array
+ * Return: center of partitions
+ */
+int partition(int *arr, int low, int high, size_t size)
+{
+	int pivot = arr[high], temp;
+	int i = (low - 1), j;
+
+	for (j = low; j <= high - 1; j++)
+	{
+		if (arr[j] <= pivot)
+		{
+			i++;
+			temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+			print_array(arr, size);
+		}
+	}
+	temp = arr[i + 1];
+	arr[i + 1] = arr[high];
+	arr[high] = temp;
+	print_array(arr, size);
+
+	return (i + 1);
+}
+
+/**
+ * quick_sort_helper - sort array using quicksort (Lomuto partioning)
+ * @array: ...
+ * @low: start of partition
+ * @high: end of partitions
+ * @size: size of array
+ */
+void quick_sort_helper(int *array, int low, int high, size_t size)
+{
+	int pivot;
+
+	if (high <= low)
+		return;
+
+	/* printf("low: %d, high %d\n", low, high) ;*/
+
+	pivot = partition(array, low, high, size);
+
+	quick_sort_helper(array, low, pivot - 1, size);
+	quick_sort_helper(array, pivot + 1, high, size);
+}
+
+/**
+ * quick_sort - sort array using quicksort (Lomuto partioning)
+ * @array: ...
+ * @size: ...
+ */
+void quick_sort(int *array, size_t size)
+{
+	quick_sort_helper(array, 0, size - 1, size);
+}
--- sort.h
+++ sort.h
@@ -22,6 +22,9 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 listint_t *create_listint(const int *array, size_t size);
 void selection_sort(int *array, size_t size);
+
+int partition(int *arr, int low, int high, size_t size);
+void quick_sort_helper(int *array, int low, int high, size_t size);
 void quick_sort(int *array, size_t size);
 
 #endif /* SORT_H */
